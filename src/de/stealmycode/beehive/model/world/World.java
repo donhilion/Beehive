64fc4f7bd2573dcd9f8e34e1c16b42621aa36d76
Ivo Senner <ivo.senner@googlemail.com>
2012-04-28 13:52:49 +0200

    Create IDrawable for every drawable Object

--- src/de/stealmycode/beehive/model/world/World.java
+++ src/de/stealmycode/beehive/model/world/World.java
@@ -1,5 +1,21 @@
 package de.stealmycode.beehive.model.world;
 
 public class World {
+	private int width = 200;
+	private int height = 200;
+	private Field[][] map = null;
+	
+	public World(int width, int height) {
+		map = new Field[width][height];
+	}
 
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
+	
 }
---
+++ src/de/stealmycode/beehive/utils/Position.java
@@ -0,0 +1,6 @@
+package de.stealmycode.beehive.utils;
+
+public class Position {
+	int x;
+	int y;
+}
