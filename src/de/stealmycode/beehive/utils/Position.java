045b110d97d43bd078ffd027196801c975eb7271
Benjamin Kirchner <benjamin.kirchner@mni.thm.de>
2012-04-28 18:42:47 +0200

    Added getter to position.

--- src/de/stealmycode/beehive/utils/Position.java
+++ src/de/stealmycode/beehive/utils/Position.java
@@ -1,11 +1,19 @@
 package de.stealmycode.beehive.utils;
 
 public class Position {
-	int x;
-	int y;
+	private int x;
+	private int y;
 	
 	public Position(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
 	}
 }
