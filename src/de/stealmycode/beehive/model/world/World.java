65c5f95bc2e6c1f26fd4c1885973c272be627a50
Fabian Müller <fmueller83@googlemail.com>
2012-04-28 16:56:45 +0200

    Introduced IWorld interface.

--- src/de/stealmycode/beehive/model/world/World.java
+++ src/de/stealmycode/beehive/model/world/World.java
@@ -4,15 +4,15 @@ import java.util.Random;
 
 import de.stealmycode.beehive.utils.Position;
 
-public class World {
+public class World implements IWorld {
 	private static float DEF_PROBABILITY = 0.1f;
 	
 	private int width = 200;
 	private int height = 200;
-	private Field[][] map = null;
+	private Field[][] fields = null;
 	
 	public World(int width, int height) {
-		map = new Field[width][height];
+		fields = new Field[width][height];
 	}
 
 	public int getWidth() {
@@ -27,13 +27,18 @@ public class World {
 		Random r = new Random();
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
-				map[x][y] = new Field(new Position(x, y));
+				fields[x][y] = new Field(new Position(x, y));
 				for (FieldProperty property : AvailableProperties.getProperties()) {
 					if (r.nextFloat() <= DEF_PROBABILITY * property.probability) {
-						map[x][y].addProperty(property.clone());
+						fields[x][y].addProperty(property.clone());
 					}
 				}
 			}
 		}
 	}
+
+    @Override
+    public Field[][] getFields() {
+        return fields;
+    }
 }
