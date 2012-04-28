81cb2367316ea38f5b9d41f9280ea05eda56efa3
jx47 <jx47@gmx.de>
2012-04-28 15:14:10 +0200

    Added Fontmanager and modified version of slick

---
+++ src/de/stealmycode/beehive/graphics_engine/FontManager.java
@@ -0,0 +1,47 @@
+package de.stealmycode.beehive.graphics_engine;
+
+import java.awt.Color;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
+
+import de.stealmycode.beehive.utils.Logger;
+
+public class FontManager {
+	String fancyPath = "ressources/MysteryQuest-Regular.ttf";
+	String uncomplicatedPath = "ressources/Share-Regular.ttf";
+	
+	public boolean createFancyFont(String fancyPath){
+		try {
+			UnicodeFont fancy = new UnicodeFont(fancyPath , 20, false, false); //Create Instance
+			fancy.addAsciiGlyphs();   //Add Glyphs
+			fancy.addGlyphs(400, 600); //Add Glyphs
+			fancy.getEffects().add(new ColorEffect(Color.BLACK)); //Add Effects
+			fancy.loadGlyphs();  //Load Glyphs
+			return true;
+		} catch (Exception e) {
+				Logger.loge("Could not load fancy Font", e, this.getClass());
+				return false;
+			}
+		}
+	
+	public boolean createUncomplicatedFont(String uncomplicatedPath){
+		try {
+			UnicodeFont uncomplicated = new UnicodeFont(uncomplicatedPath , 20, false, false); //Create Instance
+			uncomplicated.addAsciiGlyphs();   //Add Glyphs
+			uncomplicated.addGlyphs(400, 600); //Add Glyphs
+			uncomplicated.getEffects().add(new ColorEffect(Color.BLACK)); //Add Effects
+			uncomplicated.loadGlyphs();  //Load Glyphs
+			return true;
+		} catch (Exception e) {
+				Logger.loge("Could not load uncomplicated Font", e, this.getClass());
+				return false;
+			}
+		}
+	/**Usage of the fonts:
+	 * public void enterName(String s){
+  		fancy.addGlyphs(s);
+  		fancy.load();
+		}
+	 */
+}
+
