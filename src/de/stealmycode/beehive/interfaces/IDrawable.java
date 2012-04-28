13a272c5742d990e6c6b4092057c97aa4bdac0c8
Ivo Senner <ivo.senner@googlemail.com>
2012-04-28 17:47:22 +0200

    Dont need progress for IDrawable

--- src/de/stealmycode/beehive/interfaces/IDrawable.java
+++ src/de/stealmycode/beehive/interfaces/IDrawable.java
@@ -16,16 +16,6 @@ public interface IDrawable {
 	 */
 	public Direction geDirection();
 	
-	
-	/**
-	 * The progress-value represents if the object is still on his old position (0),
-	 * on the way to a new position (between 0 and 1 exclusive)
-	 * or arrived the new position (1)
-	 * 
-	 * @return a float-value between 0 and 1 inclusive.
-	 */
-	public float getProgress();
-	
 	/**
 	 * 
 	 * @return The position of the object on the GUI
