cfb3f0b1be5b75c13751744fdb2f1876f95a2542
Fabian Müller <fmueller83@googlemail.com>
2012-05-01 03:17:56 +0200

    changed lib-path to relative

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp build/classes/:libs/jar/snakeyaml-1.10.jar:libs/jar/lwjgl.jar:libs/jar/slick.jar:libs/jar/lwjgl_util.jar -Djava.library.path=/home/fate/NetBeansProjects/Beehive/libs/native/linux de.stealmycode.beehive.Beehive
+java -cp build/classes/:libs/jar/snakeyaml-1.10.jar:libs/jar/lwjgl.jar:libs/jar/slick.jar:libs/jar/lwjgl_util.jar -Djava.library.path=libs/native/linux de.stealmycode.beehive.Beehive
