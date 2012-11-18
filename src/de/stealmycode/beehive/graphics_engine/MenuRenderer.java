e72c430dd1f867a0752b87b47de548fe1afa3997
maltos <maltos@maltlappy.(none)>
2012-11-18 14:13:56 +0100

    MenuRenderer now using text-elements for entrys, added possibility for color-change in case of selection, fontmanager uses HashMap

--- src/de/stealmycode/beehive/graphics_engine/MenuRenderer.java
+++ src/de/stealmycode/beehive/graphics_engine/MenuRenderer.java
@@ -1,149 +1,155 @@
 package de.stealmycode.beehive.graphics_engine;
 
-import java.util.List;
-
 import org.lwjgl.opengl.GL11;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.UnicodeFont;
-import org.newdawn.slick.font.effects.ColorEffect;
-
-import de.stealmycode.beehive.model.world.IDrawable;
-import de.stealmycode.beehive.model.world.animals.IMovable;
-import de.stealmycode.beehive.utils.Constants;
-import de.stealmycode.beehive.utils.Direction;
 import de.stealmycode.beehive.utils.Position;
-
 import de.stealmycode.beehive.graphics_engine.FontManager;
 
-
-public class MenuRenderer implements IRenderer  {
+/**
+ * 
+ * @author maltos
+ * 
+ */
+public class MenuRenderer implements IRenderer {
 
 	private int width;
 	private int height;
-	
-//	public UnicodeFont fancy;
+	private int selected = -1;
+	private String[] menuElements;
 
-	
 	private ImageManager imageManager;
-	
-	
-	public boolean init(int width, int height){
+
+	/**
+	 * Initiate MenuRenderer
+	 * 
+	 * @param width
+	 * @param height
+	 * @return
+	 */
+	public boolean init(int width, int height) {
 		this.width = width;
 		this.height = height;
-		
-//		createFancyFont(fancypath);
-		
+
 		setViewport();
 
-		
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);		
-		
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
 		return true;
 	}
-	
-	/*
-	private void drawSprite(Sprite sprite, float x, float y, float angle, boolean absolute) {
-		Color.white.bind();
-		sprite.texture.bind();
-		
-		
-	}
-	*/
 
-	private void drawHUD(Sprite sprite,float x1, float y1,float x2,float y2) throws SlickException{
+	/**
+	 * Draw's Sprites as GL_QUADS using x1,y1,x2,y2
+	 * 
+	 * @param sprite
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @throws SlickException
+	 */
+	private void drawElement(Sprite sprite, float x1, float y1, float x2, float y2)
+			throws SlickException {
 		Color.white.bind();
-		
-//		String[] hudElements = {"Start","Speichern","Laden","Ende"};
-	
 		sprite.texture.bind();
-		
-		
+
+		GL11.glPushMatrix();
 		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glTexCoord2f(sprite.x,sprite.y);
-		GL11.glVertex2f(x1, height-y1);										//oben-links
-		GL11.glTexCoord2f(sprite.x+sprite.rectWidth,sprite.y);
-		GL11.glVertex2f(x2, height-y1);										//oben-rechts
-		GL11.glTexCoord2f(sprite.x+sprite.rectWidth,sprite.y+sprite.rectHeight);
-		GL11.glVertex2f(x2, height-y2);										//unten-rechts
-		GL11.glTexCoord2f(sprite.x,sprite.y+sprite.rectHeight);
-		GL11.glVertex2f(x1, height-y2);										//unten-links
+		GL11.glTexCoord2f(sprite.x, sprite.y);
+		GL11.glVertex2f(x1, height - y1); // oben-links
+		GL11.glTexCoord2f(sprite.x + sprite.rectWidth, sprite.y);
+		GL11.glVertex2f(x2, height - y1); // oben-rechts
+		GL11.glTexCoord2f(sprite.x + sprite.rectWidth, sprite.y
+				+ sprite.rectHeight);
+		GL11.glVertex2f(x2, height - y2); // unten-rechts
+		GL11.glTexCoord2f(sprite.x, sprite.y + sprite.rectHeight);
+		GL11.glVertex2f(x1, height - y2); // unten-links
 		GL11.glEnd();
-
-//		GL11.glBegin(GL11.GL_)
-			
-
-		
+		GL11.glPopMatrix();
 	}
-	
+
+	/**
+	 * Define ImageManager
+	 * 
+	 * @param imageManager
+	 */
 	public void setImageRenderer(ImageManager imageManager) {
 		this.imageManager = imageManager;
 	}
-	
-	public void draw(){
-		try {
-		if(imageManager == null) return;
-		setViewport();
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-//		Sprite hud = imageManager.getSprite(40);
 
-
-		
-//		FontManager newf = 	;
-
-//		createFancyFont("ressources/MysteryQuest-Regular.ttf");
-		
-		
-		Sprite newgame;
-		newgame = imageManager.getSprite(39);	// königin bild
-		drawHUD(newgame, 10 , 10, 310, 310);
-		newgame = null;
-
-		newgame = imageManager.getSprite(38);	// worrior bild
-		drawHUD(newgame, 500 , 280, 800, 580);
-		newgame = null;
-		
-
-		for(int t=1;t<=5;t++){
-			newgame = null;
-			newgame = imageManager.getSprite(40+t);
-			drawHUD(newgame, (width/2)-160, 10+70*t, (width/2)+160, 10+140*t);	
-		}
-		
-		FontManager bla = new FontManager();
-//		GL11.glBegin(GL11.GL_BLEND);
-//		GL11.glScalef(1.1f, 1.1f, 1.1f);
-		bla.drawString("blabla1234567890");
-//		GL11.glEnd();
-
-//		FontManager.drawString("blabla");
-//		Sprite newgame = imageManager.getSprite(41);
-//		drawHUD(newgame, (width/2)-160, 100, (width/2)+160, 200);
-		}		
-		catch (SlickException e) {}
-		
+	/**
+	 * Set Selected Menu-Entry for highlighting
+	 * 
+	 * @param index
+	 */
+	public void setSelected(int index) {
+		this.selected = index;
 	}
 
+	/**
+	 * Defines Menu-Entrys
+	 * 
+	 * @param elements
+	 */
+	public void setMenuElements(String[] elements) {
+		this.menuElements = elements;
+	}
+
+	/**
+	 * Draw's the Menu including 2 pictures for ambiente..
+	 */
+	public void draw() {
+		try {
+			if (imageManager == null) return;
+			setViewport();
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+			Sprite spriteToDraw;
+			
+			spriteToDraw = imageManager.getSprite(39); // königin bild
+			drawElement(spriteToDraw, 10, 10, 310, 310);
+			
+			spriteToDraw = imageManager.getSprite(38); // worrior bild
+			drawElement(spriteToDraw, 500, 280, 800, 580);
+
+			
+			// GL11 needs to flip String horizontaly
+			FontManager fontMan = new FontManager();
+			GL11.glPushMatrix();
+			GL11.glScalef(1.0f, -1.0f, 1.0f);
+			for (int n = 4; n >= 0; n--) {
+				if (selected == n) {
+					fontMan.drawString(menuElements[n], (width / 2) - 160, -1
+							* height + 30 + n * 100, 60, true);
+				} else {
+					fontMan.drawString(menuElements[n], (width / 2) - 160, -1
+							* height + 30 + n * 100, 60, false);
+				}
+			}
+			GL11.glPopMatrix();
+
+		} catch (SlickException e) {
+		}
+	}
 
 	@Override
 	public void scrollX(int x) {
 		return;
 	}
-	
+
 	@Override
 	public void scrollY(int y) {
 		return;
 	}
-	
+
 	@Override
 	public Position getGamePosition(int x, int y) {
-		return new Position(x,y);
+		return new Position(x, y);
 	}
-	
+
 	private void setViewport() {
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
